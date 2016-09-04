592cb8e47a84fd0d6e069f9b520d309148687e6e
mvrbancic <marko.vrba@gmail.com>
2016-09-04 11:44:53 +0200

    Shake event complete

--- app/src/main/java/mvrbancic/naughtynaughtytwo/MainActivity.java
+++ app/src/main/java/mvrbancic/naughtynaughtytwo/MainActivity.java
@@ -1,66 +1,80 @@
 package mvrbancic.naughtynaughtytwo;
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
 import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SensorListener
+{
     private SensorManager mSensorManager;
 
-    private ShakeDetector mSensorListener;
-
+    //private vars
     ImageView image;
+    AssetFileDescriptor afd;
+    String name = "bajaga.mp3";
+    MediaPlayer player;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        image = (ImageView) findViewById(R.id.imageView);
-
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensorListener = new ShakeDetector();
-
-        mSensorListener.setOnShakeListener(new ShakeDetector.OnShakeListener() {
-
-            public void onShake() {
-                Toast.makeText(getApplicationContext(), "Shake!", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION), SensorManager.SENSOR_DELAY_NORMAL);
 
     }
+
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+
+        public void onSensorChanged(SensorEvent se) {
+
+            float shake = Math.abs(se.values[0]) * 10;
+
+            Toast.makeText(getApplicationContext(), Float.toString(shake), Toast.LENGTH_SHORT).show();
+
+            /*if(shake > 10)
+            {
+                Toast.makeText(getApplicationContext(), Float.toString(shake), Toast.LENGTH_SHORT).show();
+            }*/
+
+
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(mSensorListener,
-                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_UI);
-
-        Toast.makeText(getApplicationContext(), "Resume!", Toast.LENGTH_SHORT).show();
-
+         mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
     protected void onPause() {
         mSensorManager.unregisterListener(mSensorListener);
         super.onPause();
-
-        Toast.makeText(getApplicationContext(), "Stop!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    protected void onStop() {
-        mSensorManager.unregisterListener(mSensorListener);
-        super.onStop();
+    public void onSensorChanged(int sensor, float[] values) {
 
-        Toast.makeText(getApplicationContext(), "Stop!", Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    public void onAccuracyChanged(int sensor, int accuracy) {
 
+    }
 }
